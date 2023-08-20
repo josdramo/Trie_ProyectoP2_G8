bed0a8fb1e92cc31f992df8a0ad9c192f219c4d8
Christopher Díaz <nealthmc@gmail.com>
2023-08-19 20:27:35 -0500

    renames

---
+++ Trie_ProyectoP2_G8/src/main/java/com/main/App.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class App {
+
+    public static void main(String[] args) {
+        Trie abb = new Trie();
+
+        abb.insertarPalabra("Hola");
+        abb.insertarPalabra("hola");
+        abb.insertarPalabra("h0la");
+        abb.insertarPalabra("cono");
+        
+    }
+}
