097e0fb25b4fa413c510493a5d8cd3a8b5133dfe
Christopher Díaz <nealthmc@gmail.com>
2023-08-20 01:21:50 -0500

    add controllers

---
+++ Trie_ProyectoP2_G8/src/main/java/controllers/Controller.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import com.main.App;
+
+public class Controller {
+    private App app;
+
+    public App getApp() {
+        return app;
+    }
+
+    public void setApp(App app) {
+        this.app = app;
+    }
+}
