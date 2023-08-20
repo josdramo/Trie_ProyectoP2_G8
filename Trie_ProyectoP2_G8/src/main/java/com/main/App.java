20204dbb4ff356ce3a66a72938f3a4d5bcbf893e
Christopher Díaz <nealthmc@gmail.com>
2023-08-19 20:46:04 -0500

    install javafx

--- Trie_ProyectoP2_G8/src/main/java/com/main/App.java
+++ Trie_ProyectoP2_G8/src/main/java/com/main/App.java
@@ -1,6 +1,12 @@
 package com.main;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
 
     public static void main(String[] args) {
         Trie abb = new Trie();
@@ -10,5 +16,29 @@ public class App {
         abb.insertarPalabra("h0la");
         abb.insertarPalabra("cono");
         
+        launch(args);
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Configuración de la ventana principal
+        primaryStage.setTitle("Ejemplo JavaFX");
+        
+        // Creación de un nodo de contenido (en este caso, un Label)
+        Label label = new Label("¡Hola, JavaFX!");
+        
+        // Creación de un contenedor para el nodo de contenido
+        VBox root = new VBox();
+        root.getChildren().add(label);
+        
+        // Creación de una escena que contiene el contenedor
+        Scene scene = new Scene(root, 300, 200);
+        
+        // Asignación de la escena a la ventana principal
+        primaryStage.setScene(scene);
+        
+        // Mostrar la ventana principal
+        primaryStage.show();
     }
 }
