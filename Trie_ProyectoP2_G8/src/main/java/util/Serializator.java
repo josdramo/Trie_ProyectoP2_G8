778ed56fa0e5293ca1976f52d39f0ef2d28e65aa
Christopher Díaz <nealthmc@gmail.com>
2023-08-21 12:19:12 -0500

    Create Serializator.java

---
+++ Trie_ProyectoP2_G8/src/main/java/util/Serializator.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ *
+ * @author infrative
+ */
+public class Serializator {
+    public static <T extends Serializable> void serialize(T object, String filename) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("data/" + filename);
+            try (ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+                out.writeObject(object);
+            }
+        } catch (IOException e) {
+            System.out.println("[ERROR] Serialization.serialize");
+        }
+    }
+    
+    public static <T extends Serializable> T deserialize(String filename) {
+        FileInputStream fileIn = null;
+        try {
+            fileIn = new FileInputStream("data/" + filename);
+            T object;
+            try (ObjectInputStream in = new ObjectInputStream(fileIn)) {
+                object = (T) in.readObject();
+            }
+            
+            return object;
+        } catch (IOException | ClassNotFoundException e) {
+            //e.printStackTrace();
+            System.out.println("[ERROR] Archivo deserializable no encontrado.");
+            if (fileIn != null) {
+                try {
+                    fileIn.close();
+                } catch (IOException e1) {
+                    //e1.printStackTrace();
+                    // do nothing
+                }
+            }
+            
+            
+            return null;
+        }
+    }
+}
