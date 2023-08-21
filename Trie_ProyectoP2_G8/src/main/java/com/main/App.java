67fc2cc995cd32a22940c2b32be68560fec9c082
Christopher Díaz <nealthmc@gmail.com>
2023-08-21 12:19:29 -0500

    format

--- Trie_ProyectoP2_G8/src/main/java/com/main/App.java
+++ Trie_ProyectoP2_G8/src/main/java/com/main/App.java
@@ -7,12 +7,12 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         ViewManager manager = new ViewManager();
+        
         manager.setStage(primaryStage);
         manager.loadViews();
         manager.showMainView();
