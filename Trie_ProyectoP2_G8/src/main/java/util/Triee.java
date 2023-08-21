553bed97528c7a5433d3fc336deb424b9c1204cd
Christopher Díaz <nealthmc@gmail.com>
2023-08-21 01:03:13 -0500

    Create Triee.java

---
+++ Trie_ProyectoP2_G8/src/main/java/util/Triee.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.util.HashMap;
+
+public class Triee {
+    private TrieNode root;
+
+    public Triee() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode currentNode = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            currentNode.children.putIfAbsent(ch, new TrieNode());
+            currentNode = currentNode.children.get(ch);
+        }
+        currentNode.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode currentNode = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!currentNode.children.containsKey(ch)) {
+                return false;
+            }
+            currentNode = currentNode.children.get(ch);
+        }
+        return currentNode.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode currentNode = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            if (!currentNode.children.containsKey(ch)) {
+                return false;
+            }
+            currentNode = currentNode.children.get(ch);
+        }
+        return true;
+    }
+}
+
+class TrieNode {
+    HashMap<Character, TrieNode> children;
+    boolean isEndOfWord;
+
+    TrieNode() {
+        children = new HashMap<>();
+        isEndOfWord = false;
+    }
+}
